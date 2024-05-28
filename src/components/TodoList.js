1e5730083ca6dceea3aadf8c7e6212fde9f7fdc8
kiran-21 <kiranksr.sr@gmail.com>
2024-05-28 12:40:15 -0400

    Modal for delete confirm  is added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -13,37 +13,49 @@ import {
   ModalCloseButton,
   Button,
   Input,
-  Divider,
 } from "@chakra-ui/react";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import React, { useState } from "react";
 
 function TodoList({ todos, deleteTodo, editTodo }) {
-  const [todo, setTodo] = useState("");
   const [modalValue, setModalValue] = useState({});
   const [isOpen, setIsOpen] = useState(false);
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+  const [deleteTodoId, setDeleteTodoId] = useState(null);
 
   function onClose() {
     setIsOpen(false);
   }
 
+  function onDeleteClose() {
+    setIsDeleteOpen(false);
+  }
+
   function handleEditClick(todo) {
     setIsOpen(true);
     setModalValue(todo);
-    console.log(todo);
   }
 
-  function handleEditInputChange(e, id) {
+  function handleEditInputChange(e) {
     setModalValue({ ...modalValue, text: e.target.value });
-    console.log(modalValue, id);
   }
 
   function handleEditSubmit(e) {
     e.preventDefault();
-
     editTodo(modalValue.id, modalValue);
-    setModalValue("");
+    setModalValue({});
     setIsOpen(false);
+  }
+
+  function handleDeleteClick(id) {
+    setIsDeleteOpen(true);
+    setDeleteTodoId(id);
+  }
+
+  function handleConfirmDelete() {
+    deleteTodo(deleteTodoId);
+    setDeleteTodoId(null);
+    setIsDeleteOpen(false);
   }
 
   return !todos.length ? (
@@ -53,7 +65,7 @@ function TodoList({ todos, deleteTodo, editTodo }) {
   ) : (
     <VStack>
       {todos.map((todo) => (
-        <HStack spacing="24px" w="320px">
+        <HStack spacing="24px" w="320px" key={todo.id}>
           <Flex p={6} w="300px" h="50px" justifyContent="space-between">
             <Text>{todo.text}</Text>
 
@@ -61,42 +73,61 @@ function TodoList({ todos, deleteTodo, editTodo }) {
               <DeleteIcon
                 color="red.500"
                 mr="2"
-                onClick={() => deleteTodo(todo.id)}
+                onClick={() => handleDeleteClick(todo.id)}
               />
               <EditIcon onClick={() => handleEditClick(todo)} />
             </Flex>
-
-            {/* modal for editing a todo */}
-            <Modal isOpen={isOpen} onClose={onClose}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>Edit Your Todo</ModalHeader>
-                <ModalCloseButton />
-                <form onSubmit={handleEditSubmit}>
-                  <ModalBody>
-                    <Input
-                      value={modalValue.text}
-                      key={modalValue.id}
-                      variant="outline"
-                      type="text"
-                      placeholder="Update your todo..."
-                      onChange={handleEditInputChange}
-                    />
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button colorScheme="teal" mr={3} onClick={onClose}>
-                      Close
-                    </Button>
-                    <Button type="submit" colorScheme="teal" mr={3}>
-                      Update
-                    </Button>
-                  </ModalFooter>
-                </form>
-              </ModalContent>
-            </Modal>
           </Flex>
         </HStack>
       ))}
+
+      {/* Modal for editing a todo */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit Your Todo</ModalHeader>
+          <ModalCloseButton />
+          <form onSubmit={handleEditSubmit}>
+            <ModalBody>
+              <Input
+                value={modalValue.text}
+                variant="outline"
+                type="text"
+                placeholder="Update your todo..."
+                onChange={handleEditInputChange}
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="teal" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button type="submit" colorScheme="teal" mr={3}>
+                Update
+              </Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+
+      {/* Modal for deleting the task*/}
+      <Modal isOpen={isDeleteOpen} onClose={onDeleteClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Confirm Delete</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Are you sure you want to delete this todo?
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="red" mr={3} onClick={handleConfirmDelete}>
+              Confirm
+            </Button>
+            <Button variant="ghost" onClick={onDeleteClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </VStack>
   );
 }
